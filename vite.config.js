f0df3fd6be0d97a754aa4e4e7f589aaa76ad29af
phuongdongiot-gif <phuongdongiot@gmail.com>
2026-03-24 11:50:46 +0700

    feat: Configure Vite build to support multiple HTML entry points and custom output asset naming.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html'),
+        values: resolve(__dirname, 'values.html'),
+        architecture: resolve(__dirname, 'architecture.html'),
+        amenities: resolve(__dirname, 'amenities.html'),
+        services: resolve(__dirname, 'services.html'),
+        floorplans: resolve(__dirname, 'floorplans.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+      output: {
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
