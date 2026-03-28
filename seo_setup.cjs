f1d72fb26b929e5270716793b70a940424d3cbca
phuongdongiot-gif <phuongdongiot@gmail.com>
2026-03-28 12:42:49 +0700

    Update seo_setup.cjs
    feat: change domain seo

--- seo_setup.cjs
+++ seo_setup.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const domain = 'https://alizedanang.vn';
+const domain = 'https://alizedanang.net';
 const dir = 'd:\\Iworkspaces\\alize-template';
 
 // 1. Fix the duplicate HTML in index.html
